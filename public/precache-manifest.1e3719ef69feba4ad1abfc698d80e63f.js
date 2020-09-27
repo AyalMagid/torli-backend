cada9fb7914a42040df81fcab10ea990de3bea94
AyalMagid <ayal.magid@gmail.com>
2020-09-27 12:35:28 +0300

    fdf

---
+++ public/precache-manifest.1e3719ef69feba4ad1abfc698d80e63f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc231baa542cde73dbdbc0cd6ed73651",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dfe3d2ae30eff31b174",
+    "url": "/static/css/main.aa7160de.chunk.css"
+  },
+  {
+    "revision": "e3923827dd7f32d66a3d",
+    "url": "/static/js/2.832b441b.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.832b441b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dfe3d2ae30eff31b174",
+    "url": "/static/js/main.9f695901.chunk.js"
+  },
+  {
+    "revision": "2220842a8d8c60b907e5",
+    "url": "/static/js/runtime-main.8457443a.js"
+  },
+  {
+    "revision": "3f5fef64b9bb3ef6ded20c917f3082f7",
+    "url": "/static/media/Heebo-Regular.3f5fef64.ttf"
+  }
+]);
