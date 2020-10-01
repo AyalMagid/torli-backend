7c7aee72a71ad85b1e86983ff987552653b1c9db
AyalMagid <ayal.magid@gmail.com>
2020-10-01 20:23:06 +0300

    sdfsd

---
+++ public/precache-manifest.ba0b5831384f665460de0f3422c0cf6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1b1583bff7689887647c7f67fa5ae2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf9392ecbe9286c13a94",
+    "url": "/static/css/main.6402a1b2.chunk.css"
+  },
+  {
+    "revision": "ed0e20a84d4717f05780",
+    "url": "/static/js/2.d9ed6690.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.d9ed6690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf9392ecbe9286c13a94",
+    "url": "/static/js/main.26d9b96c.chunk.js"
+  },
+  {
+    "revision": "2220842a8d8c60b907e5",
+    "url": "/static/js/runtime-main.8457443a.js"
+  },
+  {
+    "revision": "3f5fef64b9bb3ef6ded20c917f3082f7",
+    "url": "/static/media/Heebo-Regular.3f5fef64.ttf"
+  },
+  {
+    "revision": "34ad6669ac2e5f0d9d9fcfe96f5beb71",
+    "url": "/static/media/handlight.34ad6669.png"
+  }
+]);
