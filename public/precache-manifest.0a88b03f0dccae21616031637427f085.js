d6e51aaeb6a34da5c98d6806928c8651ade5bc84
AyalMagid <ayal.magid@gmail.com>
2020-10-29 16:01:37 +0200

    dssd

---
+++ public/precache-manifest.0a88b03f0dccae21616031637427f085.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "175e29b3ee6a24c67df57a159494110d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76b9b1eefb685f3e47b2",
+    "url": "/static/css/main.178b6237.chunk.css"
+  },
+  {
+    "revision": "0fe9338c638d547efa4b",
+    "url": "/static/js/2.f73cd2fe.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.f73cd2fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76b9b1eefb685f3e47b2",
+    "url": "/static/js/main.b5fa7a69.chunk.js"
+  },
+  {
+    "revision": "2220842a8d8c60b907e5",
+    "url": "/static/js/runtime-main.8457443a.js"
+  },
+  {
+    "revision": "c5d54929c418e69868b42a4d75063958",
+    "url": "/static/media/Alef-Regular.c5d54929.ttf"
+  },
+  {
+    "revision": "d3bebba97d549436fd6137d05d55ae33",
+    "url": "/static/media/DancingScript-VariableFont_wght.d3bebba9.ttf"
+  },
+  {
+    "revision": "3f5fef64b9bb3ef6ded20c917f3082f7",
+    "url": "/static/media/Heebo-Regular.3f5fef64.ttf"
+  },
+  {
+    "revision": "34ad6669ac2e5f0d9d9fcfe96f5beb71",
+    "url": "/static/media/handlight.34ad6669.png"
+  },
+  {
+    "revision": "1b35ebf604c198e1bb355afe43ad5899",
+    "url": "/static/media/hex3.1b35ebf6.png"
+  },
+  {
+    "revision": "13d5f0737a15acf238a44202114c6356",
+    "url": "/static/media/logo3.13d5f073.png"
+  },
+  {
+    "revision": "bb4ea92ab11b493ffe456edbd0e1f2e3",
+    "url": "/static/media/oo.bb4ea92a.png"
+  }
+]);
